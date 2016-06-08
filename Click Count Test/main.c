a30ff7320c01d6f2e390d663a84b46b549998431
Patrick Cote <pcote@mtech.edu>
2016-06-08 14:57:20 -0600

    Fixed PWM Output port to 2.4, created reels_init() function

--- Click Count Test/main.c
+++ Click Count Test/main.c
@@ -7,28 +7,33 @@ int main(void) {
 
 	WDTCTL = WDTPW | WDTHOLD;
 
-	P2DIR = BIT1;
-	P2OUT = 0x00;
-	P2SEL = BIT1;
-
-	P2DIR &= ~BIT0;
-	P2IE |=  BIT0;                            // P2.0 interrupt enabled
-	P2IES |= BIT0;                            // P2.0 Hi/lo edge
-	P2REN |= BIT0;                            // Enable Pull Up on SW2 (P2.0)
-	P2IFG &= ~BIT0;                           // P2.0 IFG clear
-
-	TA1CTL = TASSEL_2 + MC_1;
-	TA1CCTL1 |= OUTMOD_7;
-	TA1CCR0 = 21000;
-	TA1CCR1 = 1500;
+	init_reel();
 
 	__bis_SR_register(GIE);
 
 	return 0;
 }//main()
 
+void init_reel(){
+
+	//Limit Switch in
+	P2DIR &= ~BIT0;				// Limit switch input on 2.0
+	P2IE |=  BIT0;				// P2.0 interrupt enabled
+	P2IES |= BIT0;				// P2.0 Hi/lo edge
+	P2REN |= BIT0;				// Enable Pull Up on SW2 (P2.0)
+	P2IFG &= ~BIT0;				// P2.0 IFG clear
+	//Motor PWM Out
+	P2DIR |= BIT4;				//PWM Output on P2.4
+	P2SEL |= BIT4;				//TA1.2 Output on P2.4
+	TA1CTL = TASSEL_2 + MC_1;	//Set Clock
+	TA1CCTL2 |= OUTMOD_7;		//Output mode
+	TA1CCR0 = 21000;
+	TA1CCR2 = 1580;
+
+}//init_reels()
+
 int motorDir(int direction){
-	TA1CCR1 = 1500 + direction*200;
+	TA1CCR2 = 1580 + direction*200;
 }// motorDirection()
 
 // Port 2 interrupt service routine
