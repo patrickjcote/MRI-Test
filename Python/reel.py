0a2e9282d1d04e4601df7df3b4ec3af567d92637
Pi <pcote@mtech.edu>
2016-06-15 00:38:50 +0000

    Reel class pretty much functional

--- Python/reel.py
+++ Python/reel.py
@@ -1,11 +1,11 @@
 import smbus
+import time
 
-
-class REEL:
+class Reel:
     
-    def __init__(self):
+    def __init__(self, address):
         self.bus = smbus.SMBus(1)
-        self.address = 0x48
+        self.address = address
 
     def write(self,cmd):
         self.bus.write_i2c_block_data(self.address, 0 , map(ord,cmd))
@@ -13,3 +13,34 @@ class REEL:
     def read(self):
         self.readArray =  self.bus.read_i2c_block_data(self.address, 3)
         print chr(self.readArray[0])+chr(self.readArray[1])+chr(self.readArray[2])
+    
+    def rd(self,depth):
+        if depth < 10:
+             cmd = 'RD00'+str(depth)
+        elif depth < 100:
+             cmd = 'RD0'+str(depth)
+        elif depth < 1000:
+             cmd = 'RD'+str(depth)
+        self.bus.write_i2c_block_data(self.address, 0 , map(ord,cmd))
+
+    def cd(self):
+        self.bus.write_i2c_block_data(self.address, 0 , map(ord,'CD'))
+        time.sleep(.01)
+        self.read()
+
+    def q(self):
+        self.bus.write_i2c_block_data(self.address, 0 , map(ord,'Q'))
+        time.sleep(.01)
+        self.read()
+
+    def pu(self):
+        self.bus.write_i2c_block_data(self.address, 0 , map(ord,'PU'))
+
+    def s(self):
+        self.bus.write_i2c_block_data(self.address, 0 , map(ord,'S'))
+
+    def status(self):
+        print('Current depth:')
+        self.cd()
+        print('Status Code:')
+        self.q()
