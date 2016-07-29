513e26681a0e91a2a49bdc6c94d1feecd479a8b3
Patrick Cote <pcote@mtech.edu>
2016-07-28 22:38:49 -0600

    Repo cleanup

---
+++ Purge Valve/Debug/subdir_vars.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../i2c.c \
+../main.c \
+../serial_handler.c \
+../spi_bb.c 
+
+OBJS += \
+./i2c.obj \
+./main.obj \
+./serial_handler.obj \
+./spi_bb.obj 
+
+C_DEPS += \
+./i2c.pp \
+./main.pp \
+./serial_handler.pp \
+./spi_bb.pp 
+
+C_DEPS__QUOTED += \
+"i2c.pp" \
+"main.pp" \
+"serial_handler.pp" \
+"spi_bb.pp" 
+
+OBJS__QUOTED += \
+"i2c.obj" \
+"main.obj" \
+"serial_handler.obj" \
+"spi_bb.obj" 
+
+C_SRCS__QUOTED += \
+"../i2c.c" \
+"../main.c" \
+"../serial_handler.c" \
+"../spi_bb.c" 
+
+
