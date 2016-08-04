eac91019b788d5d1efa76a1b8161225bf5200998
Patrick Cote <pcote@mtech.edu>
2016-08-04 11:30:42 -0600

    Updated code for Robotics board

--- Stepper Testing/main.c
+++ Stepper Testing/main.c
@@ -11,7 +11,7 @@
 #define MIN_STEPS 10
 
 void setDirection();
-void findHome(void);
+int findHome(void);
 volatile int stepper_pos, stepper_dir, stepper_en, raster_dir;
 
 int main(void) {
@@ -26,17 +26,17 @@ int main(void) {
 	P1OUT |= BIT3 + BIT4;
 
 	// Stepper PWM
-	P2DIR |= BIT2;
-	P2SEL |= BIT2;
-	TA1CCR0 = 16000;			// 1kHz
-	TA1CCR1 = 1600;				// 10% duty
-	TA1CCTL1 = OUTMOD_7;
-	TA1CCTL0 = CCIE;			// Interrupt Enable
-	TA1CTL = TASSEL_2 + MC_1;	// smclock and count up
+	P1DIR |= BIT2;
+	P1SEL |= BIT2;
+	TA0CCR0 = 16000;			// 1kHz
+	TA0CCR1 = 1600;				// 10% duty
+	TA0CCTL1 = OUTMOD_7;
+	TA0CCTL0 = CCIE;			// Interrupt Enable
+	TA0CTL = TASSEL_2 + MC_1;	// smclock and count up
 
 	// Stepper Limit Switch
-	P2DIR &= ~BIT1;				// Limit switch input on 2.1
-	P2REN |= BIT1;				// Enable Pull Up (P1.4)
+	P1DIR &= ~BIT5;				// Limit switch input on 2.1
+	P1REN |= BIT5;				// Enable Pull Up (P1.4)
 
 	stepper_pos = 0;
 	stepper_dir = STEP_BACKWARD;
@@ -68,8 +68,6 @@ int main(void) {
 void setDirection(int direction){
 
 	volatile int n;
-	P2SEL &= ~BIT1;			//Disable steps
-
 	stepper_dir = direction;
 
 	if(direction == STEP_FORWARD)
@@ -78,15 +76,15 @@ void setDirection(int direction){
 		P1OUT |= BIT3;		// Set Direction
 
 	for(n=0;n<100;n++);		// ~5us delay
-	P2SEL |= BIT1;			// Enable steps
+
 
 }// setDirection()
 
-void findHome(void)
+int findHome(void)
 {
 	setDirection(STEP_BACKWARD);
 
-	while(P2IN & BIT1)
+	while(P1IN & BIT5)
 	{
 		stepper_dir = STEP_BACKWARD;
 	}
@@ -95,11 +93,13 @@ void findHome(void)
 	setDirection(STEP_FORWARD);
 	stepper_en = 0;
 	raster_dir = RASTER_FORWARD;
+	return 1;
 
 }// findHome()
 
+
 // Timer A0 interrupt service routine
-#pragma vector=TIMER1_A0_VECTOR
+#pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A (void)
 {
 	// Enable and Direction checks
@@ -107,7 +107,6 @@ __interrupt void Timer_A (void)
 		P1OUT |= BIT4;		// Enable Motor
 	else
 		P1OUT &= ~BIT4;		// Disable Motor
-
 
 	if(stepper_en){
 		stepper_pos += stepper_dir;	// Inc/Dec stepper position
