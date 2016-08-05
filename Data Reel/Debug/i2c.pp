0edda87cfb8b5b00c21a3a08f6e36888698c92ec
Patrick Cote <pcote@mtech.edu>
2016-08-05 11:58:34 -0600

    Clean Up

---
+++ Data Reel/Debug/i2c.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+i2c.obj: ../i2c.c
+i2c.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+i2c.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+i2c.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics_legacy_undefs.h
+
+../i2c.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics_legacy_undefs.h: 
---
+++ Data Reel/Debug/spi_bb.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+spi_bb.obj: ../spi_bb.c
+spi_bb.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+spi_bb.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+spi_bb.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+spi_bb.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics.h
+spi_bb.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics_legacy_undefs.h
+
+../spi_bb.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics_legacy_undefs.h: 
