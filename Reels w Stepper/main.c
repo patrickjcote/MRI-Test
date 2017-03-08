b4d89b383a1ae9dcdaf66d71ee56b49bd6f496a0
Patrick Cote <pcote@mtech.edu>
2017-03-08 12:55:53 -0700

    Improve stepper command codes

--- Reels w Stepper/main.c
+++ Reels w Stepper/main.c
@@ -121,7 +121,7 @@ int input_handler (char *instring, char *outstring){
 		}
 		break;
 	case 'P':
-		if (instring[1]=='U'){				// Pull Up Reel
+		if (instring[1]=='U'){					// Pull Up Reel
 			reel.setClick = -6;
 			reel.flag = 1;
 			reel.timeout1 = 0;
@@ -131,59 +131,70 @@ int input_handler (char *instring, char *outstring){
 			allStopFlag=0;
 		}
 		break;
-	case 'H':								// Send stepper motor home
-		stepper.flag = 1;
-		findHome();
-		break;
-	case 'F':								// Move stepper motor forward one position
-		stepper.flag = 1;
-		stepper.setPos = stepper.position + 1;
-		allStopFlag=0;
-		break;
-	case 'B':								// Move stepper motor backward one position
-		stepper.flag = 1;
-		stepper.setPos = stepper.position - 1;
-		allStopFlag=0;
-		break;
-	case 'L':								// Return Current stepper position
-		num2str(stepper.position,outstring,3);
-		retval=3;
-		break;
-	case 'K':								// Return Current stepper set position
-		num2str(stepper.setPos,outstring,3);
-		retval=3;
-		break;
-	case 'E':								// Go To step
-		stepper.setPos = str2num(instring+2,3);
-		stepper.flag = 1;
-		allStopFlag=0;
-		break;
-	case 'W':								// Return Current stepper set position
-		num2str(reel.currentWrap,outstring,3);
-		retval=3;
-		break;
-	case 'S':								// All Stop
-		allStopFlag=1;
-		all_stop_fun();
-		retval=0;
-		break;
-	case 'I':								// Comms check for web interface
-		outstring[0]= 'O';
-		outstring[1]= 'k';
-		retval=2;
-		break;
-	case 'Q':								// Query status
-		outstring[0]=(0x30+allStopFlag);
-		outstring[1]=(0x30+interruptCode);
-		outstring[2]=(0x30+statusCode);
-		retval=3;
-		break;
-	default:								// Default: return command bytes received
-		outstring[0]= instring[0];
-		outstring[1]= instring[1];
-		outstring[2]= instring[2];
-		retval=3;
-		break;
+
+	case 'L': // Linear Stage Control -----------------------------------------------
+		switch(instring[1]){
+		case 'H':								// Send stepper motor home
+			stepper.flag = 1;
+			findHome();
+			break;
+		case 'P':								// Go To step
+			stepper.setPos = str2num(instring+2,3);
+			stepper.flag = 1;
+			allStopFlag=0;
+			break;
+		case 'F':								// Move stepper motor forward one position
+			stepper.flag = 1;
+			stepper.setPos = stepper.position + 1;
+			allStopFlag=0;
+			break;
+		case 'B':								// Move stepper motor backward one position
+			stepper.flag = 1;
+			stepper.setPos = stepper.position - 1;
+			allStopFlag=0;
+			break;
+		case 'C':								// Return Current stepper position
+			num2str(stepper.position,outstring,3);
+			retval=3;
+			break;
+		case 'S':								// Return stepper set position
+			num2str(stepper.setPos,outstring,3);
+			retval=3;
+			break;
+		default:
+			outstring[0]= instring[0];
+			outstring[1]= instring[1];
+			outstring[2]= instring[2];
+			retval=3;
+			break;
+		}
+		break; // end of Linear stage controlls
+		case 'W':								// Return Current wrap of the reel
+			num2str(reel.currentWrap,outstring,3);
+			retval=3;
+			break;
+		case 'S':								// All Stop
+			allStopFlag=1;
+			all_stop_fun();
+			retval=0;
+			break;
+		case 'I':								// Comms check for web interface
+			outstring[0]= 'O';
+			outstring[1]= 'k';
+			retval=2;
+			break;
+		case 'Q':								// Query status
+			outstring[0]=(0x30+allStopFlag);
+			outstring[1]=(0x30+interruptCode);
+			outstring[2]=(0x30+statusCode);
+			retval=3;
+			break;
+		default:								// Default: return command bytes received
+			outstring[0]= instring[0];
+			outstring[1]= instring[1];
+			outstring[2]= instring[2];
+			retval=3;
+			break;
 	}
 
 	return retval;
